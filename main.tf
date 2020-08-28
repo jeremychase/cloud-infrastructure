e3b5fb6cf1d5bfbbfd6cdc488d724b207c8eaa73
Jeremy Chase <jeremy.chase@gmail.com>
2020-08-28 13:35:32 -0400

    Update gcp free instance from imported to terraform created

--- main.tf
+++ main.tf
@@ -25,59 +25,49 @@ provider "google" {
   region      = "us-east1"
 }
 
-resource "google_compute_instance" "gcp_free" {
+# BUG(high) split this out
+# BUG(low) check bill
+resource "google_compute_instance" "free" {
   machine_type = "f1-micro"
-  name         = "instance-1"
+  name         = "free"
+  zone         = "us-east1-b"
 
   boot_disk {
     auto_delete = true
-    device_name = "instance-1"
-    mode        = "READ_WRITE"
-    source      = "https://www.googleapis.com/compute/v1/projects/jeremychase-io/zones/us-east1-b/disks/instance-1"
 
     initialize_params {
-      image  = "https://www.googleapis.com/compute/v1/projects/debian-cloud/global/images/debian-9-stretch-v20190116"
-      labels = {}
-      size   = 30
-      type   = "pd-standard"
+      image = "debian-cloud/debian-10"
+      size  = 30
+      type  = "pd-standard"
     }
   }
 
+  metadata = {
+    startup-script = file("${path.module}/files/debian-10-startup.sh")
+  }
+
   network_interface {
-    network            = "https://www.googleapis.com/compute/v1/projects/jeremychase-io/global/networks/default"
-    network_ip         = "10.142.0.2"
-    subnetwork         = "https://www.googleapis.com/compute/v1/projects/jeremychase-io/regions/us-east1/subnetworks/default"
-    subnetwork_project = "jeremychase-io"
+    network = "default"
 
     access_config {
-      nat_ip       = "104.196.16.178"
-      network_tier = "PREMIUM"
+      // Ephemeral IP
     }
   }
 
   service_account {
-    email = "845966985833-compute@developer.gserviceaccount.com"
-    scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring.write",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/trace.append",
-    ]
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
   }
 
   lifecycle {
     prevent_destroy = true
   }
-
 }
 
 resource "aws_route53_record" "us-east1_gcp_jeremychase_io_a" {
   zone_id = aws_route53_zone.jeremychase_io.zone_id
   name    = "us-east1.gcp.jeremychase.io."
   records = [
-    "${google_compute_instance.gcp_free.network_interface[0].access_config[0].nat_ip}",
+    "${google_compute_instance.free.network_interface[0].access_config[0].nat_ip}",
   ]
   ttl  = 300
   type = "A"
